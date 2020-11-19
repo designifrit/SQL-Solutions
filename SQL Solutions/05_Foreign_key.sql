090b1d8952f200073dc1ad68d9d1314b92b2b15c
Alejandro Orozco <designifrit@gmail.com>
2020-11-19 18:36:37 -0500

    SQL Solutions

---
+++ SQL Solutions/05_Foreign_key.sql
@@ -0,0 +1,12 @@
+
+USE CentroMedico
+GO
+
+
+ALTER TABLE Paciente
+ADD FOREIGN KEY(idPais) REFERENCES Pais(idPais)
+
+
+ALTER TABLE HistoriaPaciente
+ADD FOREIGN KEY(idPaciente) REFERENCES Paciente(idPaciente)
+
