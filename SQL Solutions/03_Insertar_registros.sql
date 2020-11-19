218fc330f1b5af6eeb0f02b6ac5604a323e1c1d9
Alejandro Orozco <designifrit@gmail.com>
2020-11-19 18:36:09 -0500

    up

--- SQL Solutions/03_Insertar_registros.sql
+++
@@ -1,9 +0,0 @@
-
-USE CentroMedico
-GO
-
-INSERT INTO TurnoEstado VALUES(0, 'Pendiente')
-INSERT INTO TurnoEstado VALUES(1, 'Realizado')
-INSERT INTO TurnoEstado VALUES(2, 'Cancelado')
-
-SELECT * FROM TurnoEstado
